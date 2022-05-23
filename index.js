e29451d7f4b4fda246c2e9e061358ef3d33a912a
Franco <Zalamena>
2022-05-23 21:13:02 +0100

    Fix test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = require("./google-credentials.json");
 // const splitwiseServiceAccount = process.env.SPLITWISE_SERVICE_ACCOUNT;
 initializeApp({
   credential: cert(serviceAccount)
